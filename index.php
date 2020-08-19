a4c92f5bd13005f90fc2e2a6837eca547fe5ff25
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-08-20 02:02:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,69 +5,75 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+
+
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #6495ED;
+}
+</style>
+
+
+
+
+
+
 <title>Login Page</title>
 <link rel="stylesheet" href="css/sty.css">
 </head>
-<body style="background-color:#192a56">
+
+
+     <link rel="stylesheet" href="css/mychanged.css">
+
+<title>Created conferences by conference chairs</title>
+	<link rel="stylesheet" href="css/styleNavbar.css">
+
+</head>
+<body style="background-color:#ffff">
+
+<div>
+
+
+<ul>
+  <li><a class="active" href="index.php">WebCOMS</a></li>
+	<li><a href="login.php">Login</a></li>
+	<li><a href="register.php">Register</a></li>
+	<li><a href="help.html">Help</a></li>
+	<li><a href="contactUs.html">Contact Us</a></li>
+</ul>
+
+</br></br>
+
+</div>
+
+
 	
-	<div id="main-wrapper">
-		<center>
-			<h2>Login Form</h2>
-			<img src="imgs/webc.png" class="avatar"/>
-		</center>
 	
-		<form class="myform" action="index.php" method="post">
-			<label><b>Email:</b></label><br>
-			<input name="email" type="text" class="inputvalues" placeholder="Type your email" required/><br>
-			<label><b>Password:</b></label><br>
-			<input name="password" type="password" class="inputvalues" placeholder="Type your password" required/><br>
-			<input name="login" type="submit" id="login_btn" value="Login"/><br>
-			<a href="register.php"><input type="button" id="register_btn" value="Register"/></a>
-		</form>
-		<?php
-		if(isset($_POST['login']))
-		{
-			$email=$_POST['email'];
-			$password=$_POST['password'];
-			
-			
-			//$query="select * from userinfotable WHERE email='$email' AND password='$password'";
-			
-			$select_query = mysqli_query($con, "select * from userinfotable WHERE email='$email' AND password='$password'");
-            $select_row = mysqli_fetch_assoc($select_query);
-            $qualification = $select_row['qualification'];
-			
-			//$query_run = mysqli_query($con,$query);
-			if(mysqli_num_rows($select_query)>0)
-			{
-				// valid
-				$_SESSION['email']= $email;
-				if($qualification=="Admin"){
-					header('location:adminhomepage.php');
-				}
-				else if($qualification=="Reviewer"){
-					header('location:reviewerhomepage.php');
-				}
-				else if($qualification=="Author"){
-					header('location:Author_homepage.php');
-				}
-				else if($qualification=="Conference_chair"){
-					header('location:conferencechairhomepage.php');
-				}
-				
-				
-			}
-			else
-			{
-				// invalid
-				echo '<script type="text/javascript"> alert("Invalid credentials") </script>';
-			}
-			
-		}
-		
-		
-		?>
-		
-	</div>
 </body>
 </html>
