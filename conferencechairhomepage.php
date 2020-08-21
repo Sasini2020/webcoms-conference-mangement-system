bbac53b9de9c82bce2b36f948607babb1b0e0b91
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-08-21 14:04:45 +0530

    Update conferencechairhomepage.php

--- conferencechairhomepage.php
+++ conferencechairhomepage.php
@@ -4,43 +4,64 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<style>
+body {
+  margin: 0;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 25%;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+li a {
+  display: block;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+li a.active {
+  background-color: #6495ED;
+  color: white;
+}
+
+li a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+</style>
+
 <title>Home Page</title>
 <link rel="stylesheet" href="css/sty.css">
 </head>
 <body style="background-color:#bdc3c7">
-	
-	<div id="main-wrapper">
-		<center>
-			<h2>Conference Chair Home Page</h2>
-			<h3>Welcome
-				
-			</h3>
-			<img src="imgs/webc.png" class="avatar"/>
-		</center>
-	
-		<form class="myform" action="homepage.php" method="post">
-			<a href="create_conference.php"><input type="button" id="register_btn" value="Create a Conference"/></a>
-			<br>
-			<a href="create_conference.php"><input type="button" id="register_btn" value="Define notification templates"/></a>
-			<br>
-			<a href="create_conference.php"><input type="button" id="register_btn" value="Bulk Upload User Details"/></a>
-			<br>
-			<a href="create_conference.php"><input type="button" id="register_btn" value="Send messages"/></a>
-			<br>
-			<a href="create_conference.php"><input type="button" id="register_btn" value="Add Conference Guideline Details"/></a>
 
-			<input name="logout" type="submit" id="logout_btn" value="Log Out"/><br>
 
-			
-		</form>
-		
-		<?php
-			if(isset($_POST['logout']))
-			{
-				session_destroy();
-				header('location:index.php');
-			}
-		?>
-	</div>
+
+
+
+<ul>
+  <li><a class="active" href="conferencechairhomepage.php">WebCOMS</a></li>
+  <li><a href="create_conference.php">Create a Conference</a></li>
+	<li><a href="#conferences_view.php">Define notification templates</a></li>
+	<li><a href="#authordetails.php">Bulk Upload User Details</a></li>
+	<li><a href="#help.html">Send messages</a></li>
+	<li><a href="#contactUs.html">Add Conference Guideline Details</a></li>
+	<li><a href="index.php">Log Out</a></li>
+</ul>
+
+</br></br>
+
+
+	
+	
 </body>
 </html>
