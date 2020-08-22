d6bca295a08c01bed435c4f9603a9049debaf45a
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-08-22 22:49:51 +0530

    Update help.php

--- help.php
+++ help.php
@@ -24,6 +24,7 @@
   
   <div class="about-section">
     <h1>Help Page</h1><br />
+    <h2>WebCOMS</h2>
     <p>Some text about who we are and what we do.</p>
     <p>Resize the browser window to see that this page is responsive by the way.</p>
   </div>
