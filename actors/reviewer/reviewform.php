a8e0ba25bdca38c968b0f8bdbd4e6dba32f5d8eb
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-10-15 02:17:45 +0530

    Review Form

---
+++ actors/reviewer/reviewform.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head><h1> Review Form <h1>
+</head>
+<body>
+<h4> Paper review comment </h4>
+		<div class="row">
+    <div class="col-25">
+      <label for="name">Paper Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="name" name="name" placeholder="paper name....">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="comment">comment</label>
+    </div>
+    <div class="col-75">
+      <textarea id="comment" name="comment" placeholder="comment.." style="height:200px"></textarea>
+    </div>
+  </div>
+		<input type="submit" value="submit" name="submit"></br>
+  </boy>
+  </html>
