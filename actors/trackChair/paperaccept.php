f3727ac5b80fa4c6eb625179a6714d9e14259ef7
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-10-15 00:40:06 +0530

    Paper Acceptance

---
+++ actors/trackChair/paperaccept.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h1> Paper Acceptance </h1>
+<link rel="stylesheet" href="../../css/sty.css">
+</head>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.home {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.home:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
+
+</style>
+
+<body style="background-color:#bdc3c7">
+
+<div id="main-wrapper">
+    <h2> select Paper</h2>
+  
+     <a href="trackchairhomepage.php"><button class="button home"> Back <buttton></a>
+  </form>
+</div>
+</body>
+
+</html>
