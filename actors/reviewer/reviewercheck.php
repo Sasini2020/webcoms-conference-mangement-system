342967fc8a368053635ebf1a07c52809d41dc486
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-10-15 02:22:21 +0530

    Update reviewercheck

--- actors/reviewer/reviewercheck.php
+++ actors/reviewer/reviewercheck.php
@@ -7,12 +7,7 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <center>
-    <h1> paper review comment </h1>
-    <hr/>
-   Paper Name : <?php echo $_POST("name");?> </br>
-    Comment: <?php echo $_POST["comment"];?>
-    </center>
+   
 </body>
 </html>    
 
