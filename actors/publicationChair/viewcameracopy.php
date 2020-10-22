c81b09875f56ada44fdc1253f6bc5323b597464d
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-10-22 14:03:59 +0530

    View Final Camera-ready copy submission

---
+++ actors/publicationChair/viewcameracopy.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h1>View Final Camera-ready copy submission</h1>
+<link rel="stylesheet" href="../../css/sty.css">
+</head>
+
+<body>
+    <div class="topnav">
+       <div class="search-container">
+          <select>
+            <option value="0"> select</option>
+          </selct>
+          <input type="text" placeholder="Search" name="search">
+          <button type="submit"><i class="fa fa-search"></i>Filter</button>
+       </div>
+    </div> 
+    
+ </body>
+</html>
