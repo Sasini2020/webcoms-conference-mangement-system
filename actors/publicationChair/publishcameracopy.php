9489cab867125ff180ce3cd8d5e1aa19f465c1c4
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-10-22 14:00:12 +0530

    Publish Camera ready copy Submission

---
+++ actors/publicationChair/publishcameracopy.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h1>Publish guidelines for Camera ready copy Submission</h1>
+<link rel="stylesheet" href="../../css/sty.css">
+</head>
+
+<body>
+   <div id="main-wrapper">
+       <form action="" method ="POST">
+
+            <textarea rows="4" cols="50" name="comment"> Add guideline here </textarea><br><br>
+             <input type ="submit" value="Publish"><br><br>
+
+
+             <a href="publicationchairhomepage.php"> Back </a><br><br><br>
+        </form>
+    </div>
+        
+ </body>
+</html>
