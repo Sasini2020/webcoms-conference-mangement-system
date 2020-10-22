ca50eaf47a15f40fc6d0e2140809aaf7225a76b8
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-10-22 16:28:12 +0530

    Auto-genarate procceding

---
+++ actors/publicationChair/autoproceeding.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.gen {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.gen:hover {
+  background-color: #008CBA;
+  color: white;
+}
+.down {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #008CBA;
+}
+
+.down:hover {
+  background-color: #008CBA;
+  color: white;
+}
+
+</style>
+<h1>Auto-genarate procceding</h1>
+<link rel="stylesheet" href="../../css/sty.css">
+</head>
+
+<body>
+    <div id="main-wrapper">
+    <a href="generate.php"><button class="button gen"> Generate<buttton></a>
+    
+    <a href="download.php"><button class="button down">Download<buttton></a>
+    </div> 
+    
+ </body>
+</html>
