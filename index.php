f6d98ba55ed193529dd00db8015c92ff7b9727f9
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-07 02:37:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ body, html {
 
 .hero-image {
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("try.jpg");
-  height: 50%;
+  height: 40%;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
