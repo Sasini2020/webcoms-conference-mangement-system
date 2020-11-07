0cbe5761ceb96264be6885354d86a9746261c3dc
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-11-07 16:43:08 +0530

    Update paperaccept.php

--- actors/trackChair/paperaccept.php
+++ actors/trackChair/paperaccept.php
@@ -1,43 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-<h1> Paper Acceptance </h1>
+<link rel="stylesheet" href="../../css/main_style.css">
 <link rel="stylesheet" href="../../css/sty.css">
 </head>
-<style>
-.button {
-  border: none;
-  color: white;
-  padding: 8px 16px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-}
-
-.home {
-  background-color: white; 
-  color: black; 
-  border: 2px solid #008CBA;
-}
-
-.home:hover {
-  background-color: #008CBA;
-  color: white;
-}
-
-
-</style>
 
 <body style="background-color:#bdc3c7">
 
+<nav>
+    <ul>
+      <li><a href="trackchairhomepage.php">Back</a></li>
+    </ul>
+  </nav>
+
+  <br><br>
+  <center>
+  <h1>Paper acceptance</h1>
+   <center>
+
+ <br><br>
 <div id="main-wrapper">
+   
     <h2> select Paper</h2>
   
-     <a href="trackchairhomepage.php"><button class="button home"> Back <buttton></a>
   </form>
 </div>
 </body>
