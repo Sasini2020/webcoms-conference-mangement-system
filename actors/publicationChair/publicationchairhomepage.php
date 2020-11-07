66071578490b1d6f4914609d54af95fe2acb0f11
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-11-07 15:37:56 +0530

    Publication Chair Home Page change

--- actors/publicationChair/publicationchairhomepage.php
+++ actors/publicationChair/publicationchairhomepage.php
@@ -8,81 +8,26 @@
 <html>
 <head>
 <title>Home Page</title>
-<link rel="stylesheet" href="../../css/sty.css">
+<link rel="stylesheet" href="../../css/main_style.css">
 </head>
-<style>
 
-.button {
-  border: none;
-  color: white;
-  padding: 8px 16px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  transition-duration: 0.4s;
-  cursor: pointer;
-}
-
-.one {
-  background-color: white; 
-  color: black; 
-  border: 2px solid #008CBA;
-}
-
-.one:hover {
-  background-color: #008CBA;
-  color: white;
-}
-
-
-.tree {
-  background-color: white; 
-  color: black; 
-  border: 2px solid #008CBA;
-}
-
-.tree:hover {
-  background-color: #008CBA;
-  color: white;
-}
-
-
-.two {
-  background-color: white; 
-  color: black; 
-  border: 2px solid #008CBA;
-}
-
-.two:hover {
-  background-color: #008CBA;
-  color: white;
-}
-.four {
-  background-color: white; 
-  color: black; 
-  border: 2px solid #008CBA;
-}
-
-.four:hover {
-  background-color: #008CBA;
-  color: white;
-}
-
-.wrapper {
-  margin-right: auto;
-  margin-left:  auto;
-
-  max-width: 900px;
-
-  padding-right: 8px;
-  padding-left:  5px;
-}
-
-</style>
 	
-<body style="background-color:#bdc3c7">
+<body>
+
+<nav>
+    <ul>
+      <li><a href="publishcameracopy.php">Publish Camera ready copy guidelines</a></li>
+      <li><a href="uploadcoversub.php">Upload Cover Pages and sub pages</a></li>
+      <li><a href="viewcameracopy.php">View Camera-ready copy</a></li>
+      <li><a href="autoproceeding.php">Auto-generate proceeding</a></li>
+      
+
+    </ul>
+  </nav>
+
+  <br><br>
+
+
 	
 	<div id="main-wrapper">
 		<center>
@@ -92,12 +37,7 @@
 			</h3>
 			<img src="../../imgs/webc.png" class="avatar"/>
 		</center>
-	    <div id="wrapper">
-	     <a href="publishcameracopy.php"><button class="button one">Publish Camera ready copy guidelines</button></a>
-		 <a href="uploadcoversub.php"><button class="button two" >Upload Cover Pages and sub pages</button></a>
-		 <a href="viewcameracopy.php"><button class="button tree"> View Camera-ready copy <buttton></a>
-		 <a href="autoproceeding.php"><button class="button four"> Auto-generate proceeding <buttton></a>
-		 </div>
+	   
 		<form class="myform" action="publicationchairhomepage.php" method="post">
 			<input name="logout" type="submit" id="logout_btn" value="Log Out"/><br>
 			
