bb683d10a2d19bcef56c0c2f3f0fcc206684a845
malithweerasinghe <malithrashan03@gmail.com>
2020-11-09 00:44:14 +0530

    update publishcameracopy.php

--- actors/publicationChair/publishcameracopy.php
+++ actors/publicationChair/publishcameracopy.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-<h1>Publish guidelines for Camera ready copy Submission</h1>
+<link rel="stylesheet" href="../../css/main_style.css">
 <link rel="stylesheet" href="../../css/sty.css">
 </head>
 
+ 
+<nav>
+    <ul>
+      <li><a href="publicationchairhomepage.php">Back</a></li>
+      
+    </ul>
+  </nav>
+
+  <br><br>
+
+ <center>
+   <h1>Publish guidelines for Camera ready copy Submission</h1>  
+
+  </center>
+
+<br><br>
+
+
 <body>
+
    <div id="main-wrapper">
        <form action="" method ="POST">
-
+       <center>
             <textarea rows="4" cols="50" name="comment"> Add guideline here </textarea><br><br>
+            
              <input type ="submit" value="Publish"><br><br>
-
-
-             <a href="publicationchairhomepage.php"> Back </a><br><br><br>
+       </center> 
         </form>
     </div>
         
