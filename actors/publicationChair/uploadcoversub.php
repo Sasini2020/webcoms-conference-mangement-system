8ebfab06d200033bd2a118f52bf4f838495668c0
malithweerasinghe <malithrashan03@gmail.com>
2020-11-09 23:25:40 +0530

    uplaodcoversub.php Upload cover pages and sub pages

---
+++ actors/publicationChair/uploadcoversub.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="../../css/main_style.css">
+<link rel="stylesheet" href="../../css/sty.css">
+</head>
+
+ 
+<nav>
+    <ul>
+      <li><a href="publicationchairhomepage.php">Back</a></li>
+      
+    </ul>
+  </nav>
+
+  <br><br>
+
+ <center>
+   <h1>Upload cover pages and Sub Pages </h1>  
+
+  </center>
+<br><br>
+
+
+<body>
+
+   <div id="main-wrapper">
+       <h3> Select File </h3>
+
+      <form action="uploadcoversub.php" name="file" enctype="multipart/form-data">
+          <input type="file" name="file" id="">
+          <button type="submit" name="submit">Upload</button>
+
+         
+
+
+
+
+
+
+    </div>
+        
+ </body>
+</html>
