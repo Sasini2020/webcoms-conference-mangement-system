80cf440b068ad54e68066efaa1f1e8cb70a25175
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-11 23:38:46 +0530

    Update conference_list.php

--- actors/admin/conference_list.php
+++ actors/admin/conference_list.php
@@ -15,26 +15,111 @@
     <link rel="stylesheet" href="../../css/table_style.css">
     <link rel="stylesheet" href="../../css/main_style.css">
 
+    <style>
+
+* {
+  font-family: sans-serif; /* Change your font family */
+}
+
+.content-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  border-radius: 5px 5px 0 0;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.content-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+  font-weight: bold;
+}
+
+.content-table th,
+.content-table td {
+  padding: 12px 15px;
+}
+
+.content-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.content-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.content-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+
+.content-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+.p {
+	color:white;
+}
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  
+}
+
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("try.jpg");
+  height: 30%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+</style>
+
 </head>
 <body>
+<div class="hero-image"> 
   
 	
-  <nav>
-    <ul>
-      <li><a href="adminhomepage.php">Back to Home</a></li>
-    </ul>    
-  </nav>
-  <br><br>
+<nav>
+		<ul>
+      <!--<li><a class="active" href="index.php">WebCOMS</a></li>-->
+      <li><a href="adminhomepage.php">Home</a></li>
+			<li><a href="requested_conferences.php">Requested conferences</a></li>
+			<li><a href="conference_list.php">Conference List</a></li>
+			<li><a href="authordetails.php">Author details</a></li>
+			<li><a href="conferenceChairRegistration.php">Conference Chair Registration</a></li>
+			<li>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+			&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+			&ensp;&ensp;&ensp;&ensp;&ensp;</li>
+			<li><a href="../../index.php">Log Out</a></li>
+		
 
-  <center>
-    <p><b>Conference List</b></p>
-  </center>
-
+	</nav>
   <br><br>
+  <div class="hero-text">
+</br>
+    <h5 style="font-size:30px">Conferences List</h5>
+</div>
+
+
+
+  <br><br><br><br><br><br><br><br>
 
   <div>
-   
-    <table>
+  <center>
+    <table class="content-table">
       <thead>
 	      <tr>
           <th>ID</th>
@@ -47,7 +132,7 @@
           <th>Conference Chair</th>
       	</tr>
       </thead>
-
+</center>
 	    
       <tbody>                                     
         <?php
@@ -82,7 +167,8 @@
       ?>
 
 	  </table>	
-  </div>
-
+  </div></div>
+          </br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+<h5 style="color:white; padding:20px; margin:0; text-align:center; background-color:#063247">WebCOMS @2020</h5>
 </body>
 </html>
