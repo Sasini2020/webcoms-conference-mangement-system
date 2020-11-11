e4a4bd9af3d959fddffa45310af489940aaacf51
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-11 23:37:52 +0530

    Update adminhomepage.php

--- actors/admin/adminhomepage.php
+++ actors/admin/adminhomepage.php
@@ -10,94 +10,88 @@
 	<title>Admin Home</title>
 
 	<link rel="stylesheet" href="../../css/main_style.css">
+	<link rel="stylesheet" href="../../css/style_footer.css">
+	<link rel="stylesheet" href="../../css/table_style.css">
+	
+<style>
+.p {
+	color:white;
+}
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+  
+}
 
-	<!--<link rel="stylesheet" href="css/sty.css">
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("try.jpg");
+  height: 30%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
 
-	<style>
-	ul {
-	list-style-type: none;
-	margin: 0;
-	padding: 0;
-	overflow: hidden;
-	background-color: #333;
-	}
+</style>
 
-	li {
-	float: left;
-	}
 
-	li a {
-	display: block;
-	color: white;
-	text-align: center;
-	padding: 14px 16px;
-	text-decoration: none;
-	}
-
-	li a:hover:not(.active) {
-	background-color: #111;
-	}
-
-	.active {
-	background-color: #6495ED;
-	}
-	.logout_btn{
-		margin-top:10px;
-		background-color:#DC143C;
-		padding:5px;
-		border-radius:28px;
-		color:white;
-		width:40%;
-		text-align:center;
-		font-size:18px;
-		font-weight:bold;
-		margin-bottom:20px;
-	}
-	</style>-->
 </head>
 	
 
 <body>
+<div class="hero-image"> 
 
 	<nav>
 		<ul>
 			<!--<li><a class="active" href="index.php">WebCOMS</a></li>-->
+			<li><a href="adminhomepage.php">Home</a></li>
 			<li><a href="requested_conferences.php">Requested conferences</a></li>
 			<li><a href="conference_list.php">Conference List</a></li>
 			<li><a href="authordetails.php">Author details</a></li>
 			<li><a href="conferenceChairRegistration.php">Conference Chair Registration</a></li>
-			<!--<li><a href="help.html">Help</a></li>
-			<li><a href="contactUs.html">Contact Us</a></li>-->	
-		</ul>
-		</br></br>
-	</nav>
-
-	<div>
-		<form action="adminhomepage.php" method="post">
-			<input type="submit" name="logout" value="Log Out" />
-		</form>
-		<?php
-			if(isset($_POST['logout'])){
-				session_destroy();
-				header('location:../../index.php');
-			}
-		?>
-	</div>
-	
-	<div id="main-wrapper">
-		<center>
-			<h2>Admin Home Page</h2>
-			<h3>Welcome
-				
-			</h3>
-			<img src="../../imgs/webc.png" class="avatar"/>
-		</center>
-	
-		<!--	
-		<form class="myform" action="adminhomepage.php" method="post">
+			<li>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+			&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+			&ensp;&ensp;&ensp;&ensp;&ensp;</li>
+			<li><a href="../../index.php">Log Out</a></li>
 		
-		</form>-->
 
-	</div>
+	</nav>
+	
+<div class="hero-text">
+</br>
+    <h5 style="font-size:30px">Admin</h5>
+</div>
+
+	
+	
+
+
+	
+	</br></br></br></br></br></br></br></br></br></br></br></br>
+
+<center>
+	<?php
+        include "forhomepage-reqC.php";
+	?>
+	</br></br></br></br>
+	<?php
+        include "forhomepage-conferenceL.php";
+	?>
+	</br></br></br></br>
+	<?php
+        include "forhomepage-rauthorD.php";
+	?>
+	</br></br></br></br></br></br></br></br>
+</center>
+<h5 style="color:white; padding:20px; margin:0; text-align:center; background-color:#063247">WebCOMS @2020</h5>
 </body>
 </html>
