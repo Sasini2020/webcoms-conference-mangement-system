8acd1aea0c58777712f8366eb35c5dcc20aeca29
Sasini2020 <63692107+Sasini2020@users.noreply.github.com>
2020-11-12 09:47:43 +0530

    Finished About page

---
+++ About.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>About</title>
+    <link rel="stylesheet" href="css/about_home_styles.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+
+    <nav>
+      <div class="logo">Web-COMS</div>
+      <input type="checkbox" id="click">
+            <label for="click" class="menu-btn">
+              <i class="fas fa-bars"></i>
+            </label>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a class="active" href="About.php">About</a></li>
+                <li><a href="help.php">Help</a></li>
+            </ul>
+    </nav><br><br><br><br><br><br><br><br><br><br><br>
+
+<!-- Start of the Top content -->
+    <section class="content">
+      <section>Web-COMS Conference Management System</section><br>
+      <p>Web-coms can cater to your multiple requirements proficiently.
+         Web-coms is a solution that aids in the organization of 
+         conferences. With this unique software, you can easily manage
+          members of a conference management system on one platform.
+        <br><br>With this project our effort is to
+          develop the Web-coms online conference management system to
+         handle all major processes. Its functionalities are fully 
+         accessible through web based interfaces</p>
+    </section>
+<!-- End of Top content -->
+
+<!-- Start of the bottom section -->
+<section class="footer-clearfix">
+          <h1>Our features</h1>
+
+            <div class="footcol">
+                <h3>User Roles</h3>
+
+                <!-- For actors -->
+                <div class="post">
+                  <i class="fas fa-user-shield"></i>
+                  <span>Admin</span>
+                </div>
+                <div class="post">
+                  <i class="fas fa-user-cog"></i>
+                  <span>Conference-Chair</span>
+                </div>
+                <div class="post">
+                  <i class="fas fa-user-edit"></i>
+                  <span>Author</span>
+                </div>
+                <div class="post">
+                  <i class="fas fa-user-tie"></i>
+                  <span>Reviewer</span>
+                </div>
+                <div class="post">
+                  <i class="fas fa-user"></i>
+                  <span>Track-Chair</span>
+                </div>
+                <div class="post">
+                  <i class="fas fa-user"></i>
+                  <span>Publication-Chair</span>
+                </div><br>        
+              </div><!--footcol-->
+
+            <div class="footcol">
+                <h3>Author Notification, Reviewer Suggestion</h3>
+                <p>Quick steps to notify authors using Author Notification
+                  Send notifications to authors who submitted low-quality papers</p>
+                  <p>Reviewer will check the paper submission according to his research interests
+                    , reviewer will view the review form , and reviewer sends review comments and recommendation </p>
+            </div><!--footcol-->
+
+            <div class="footcol">
+                <h3>Files and Media,Full submission cycle</h3>
+                <p>Unlimited file upload. Max file size 100MB. Support pdf for now..</p>
+                <p>Abstract, full paper, camera-ready, presentation</p>
+               
+            </div><!--footcol-->
+
+            <div class="footcol">
+              <h3>Multi Tracks</h3>
+              <!-- <div class="post">
+                  <h4>POST TITLE</h4>
+                  <p><a href="#">Admin</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, magni. Corrupti tee,nm lihtiuhcs jg7slojc yfr6sxkn</p>
+                  <a  class="readmore" href="#" >read more &raquo;</a>
+              </div>
+              <div class="post">
+                  <h4>pOST TITLE</h4>
+                  <p><a href="#">Admin</a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, magni. Corrupti tee,nm lihtiuhcs jg7slojc yfr6sxkn</p>
+                  <a  class="readmore" href="#" >read more &raquo;</a>
+              </div>post -->
+                <p>No limit on number of tracks in a conference with each track independently configured</p>
+                  <br><br>
+              </div><!--footcol-->
+          
+        </section><!-- End of the bottom section -->
+
+        <br><br>
+
+         <!-- Footer section -->
+         <div class="footer">
+            <p>&copy;2020, All rights reserved by www.WebComs.lk</p>
+         </div>
+
+</body>
+</html>
