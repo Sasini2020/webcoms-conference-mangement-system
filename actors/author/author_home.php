146e9864e221e73235d757257b1232b15ae7580c
Rashan <65862870+malithweerasinghe@users.noreply.github.com>
2020-11-14 13:37:58 +0530

    auther homepage change

--- actors/author/author_home.php
+++ actors/author/author_home.php
@@ -9,19 +9,32 @@
 <html>
 <head>
     <title>Author Home</title>
-    <link rel="stylesheet" href="../../css/main_style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+ 	<link rel="stylesheet" href="../../css/nav_footer_styles.css">
+  <!--<link rel="stylesheet" href="../../css/main_style.css">-->
 </head>
 <body>
 <nav>
     <ul>
       <li><a href="ConferenceListForA.php">Conference List</a></li>
-      <li style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
+      <li ><a style="float:right; margin-right:40px" href="../logout.php">Log Out</a></li>
     </ul>
   </nav>
 
   <br><br>
 
-    <p> Welcome Author </p><br><br>
+    <div id="main-wrapper">
+		<center>
+			<h2>Author Home Page</h2>
+			<h3> Welcome </h3>
+			<img src="../../imgs/webc.png" class="avatar"/>
+		</center>
+	   
+	</div>
+  <!-- Footer section -->
+	<div class="footer">
+            <p>&copy;2020, All rights reserved by www.WebComs.lk</p>
+        </div>
 
 </body>
 </html>
