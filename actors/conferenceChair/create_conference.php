95efc777fd8aeede6f0775b9e394807a1a854f11
Sasini <63692107+Sasini2020@users.noreply.github.com>
2020-11-14 20:51:41 +0530

    Finalized conferemce creation

--- actors/conferenceChair/create_conference.php
+++ actors/conferenceChair/create_conference.php
@@ -15,9 +15,21 @@
 	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
 	<link rel="stylesheet" href="../../css/reg_form_style.css">
-	<link rel="stylesheet" href="../../css/nav_footer_styles.css">
+	<link rel="stylesheet" href="../../css/about_help_styles.css">
 
-
+ <!-- styles for dots in paragraphs -->
+ <style>
+.dot {
+  height: 8px;
+  width: 8px;
+  background-color: #86B0DD;
+  border-radius: 50%;
+  margin-bottom:2px;
+  margin-left:28px;
+  margin-right:5px;
+  display: inline-block;
+}
+</style>
 </head>
 <body>
 
@@ -28,40 +40,66 @@
               <i class="fas fa-bars"></i>
             </label>
 		<ul>
-			<li><a class="active" href="create_conference.php">Create a Conference</a></li>
+			<li><a class="active" href="create_conference.php">Request a Conference</a></li>
 			<li><a href="conferencechairhomepage.php">Back to Home</a></li>
-			
+			<li><a href="../../help.php">Help</a></li>
+			<li><a href="../../About.php">About</a></li>
 		</ul>
 	</nav>
 
-	<br><br><br>
+	<br>
 	<div id="main-wrapper">
 		
-		<form action="create_conference.php"method="post">
-			<br><h1>Create a Conference</h1>
+		<h2 style="margin-left:25px;color:dodgerblue;">New Conference Request</h2><br>
+		<p style="margin-left:25px;margin-bottom:29px;color:#283747;font-weight:400;">In order to have a conference site in Web-COMS, you will need to request one.
+		Once you submit the request for your conference, it can take up to 72 hours for the conference site to be created.
+		Once the request is approved and the conference site created, you will receive an email with all the information pertaining to your conference. </p>
 
-			<label>Conference Name:</b></label><br>
+		<h3 style="margin-left:25px;color:dodgerblue;">Criteria For Submission</h3><br>
+		
+		<p style="margin-left:25px;margin-bottom:15px;color:#283747;font-weight:500;">
+		Before clicking "Submit" to save your request, please make sure that you use the correct information in the correct format for the conference.
+		</p>
+
+		<p style="margin-left:25px;color:#283747;font-weight:400;">
+		
+			<span class="dot"></span>
+			Enter the <span style='color: dodgerblue;font-weight:500;'>full title</span> of the conference
+			<br>
+			<span class="dot"></span>
+			<!-- I used span for color words in p tag -->
+			Use <span style='color: dodgerblue;font-weight:500;'>mm/dd/yyyy</span> format for dates (Start Date, End Date, Paper Submission Due Date) for site request.
+			For example, Feb 1st, 2021 would be 02/01/2021.
+		</p>
+		<br>
+
+		<h3 style="margin-left:25px;color:dodgerblue;">Request Form</h3>
+
+		<form action="create_conference.php"method="post">
+			<br><h1>Request a Conference</h1>
+
+			<label>Conference Name*</b></label><br>
 			<input name="name" type="text" class="inputvalues" placeholder="Type your conference's title" required/><br>
 			
-			<label>Venue:</b></label><br>
+			<label>Venue*</b></label><br>
 			<input name="venue" type="text" class="inputvalues" placeholder="Venue" required/><br>
 			
-			<label >Start date:</b></label><br>
+			<label >Start Date*</b></label><br>
 			<input name="start_date" required type="date" class="inputvalues" placeholder="dd-mm-yyyy" min="2020-12-15" required/><br>
 			
-			<label>End date:</b></label><br>
+			<label>End Date*</b></label><br>
 			<input name="end_date" type="date" class="inputvalues" placeholder="dd-mm-yyyy" min="2021-01-15" required/><br>
 			
-			<label>Dead line:</b></label><br>
+			<label>Paper Submission Due Date*</b></label><br>
 			<input name="deadline" type="date" class="inputvalues" placeholder="dd-mm-yyyy" min="2021-02-15" required/><br>
 			
-			<label>Sponsor/s details:</b></label><br>
+			<label>Sponsor/s details*</b></label><br>
 			<input name="sponsor_details" type="text" class="inputvalues" cols="30" placeholder="Sponsor's details" required/><br>
 
 			<!-- <input name="create_btn" type="submit" id="register_btn" value="CREATE"/><br> -->
-			<button name="create_btn" type="submit" id="register_btn" value="CREATE">Create</button><br>
+			<button name="create_btn" type="submit" id="register_btn" value="CREATE">Submit</button>
+			<button type="cancel" class="button" onclick="javascript:window.location='create_conference.php'">Cancel</button>
 
-			<!--<a href="conferencechairhomepage.php"><input type="button" id="back_btn" value="Back"/></a>-->
 		</form>
 		
 		<?php
