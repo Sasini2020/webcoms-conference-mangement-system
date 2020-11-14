b546a3ff58cc36fcfe6c871ca007bdc9fd878161
Sasini <63692107+Sasini2020@users.noreply.github.com>
2020-11-14 18:15:05 +0530

    Styled this viewcamera ready for publication chair

--- actors/publicationChair/viewcamerareadycopies.php
+++ actors/publicationChair/viewcamerareadycopies.php
@@ -39,7 +39,7 @@
   padding-top: 12px;
   padding-bottom: 12px;
   text-align: left;
-  background-color: #6495ED;
+  background-color: dodgerblue;
   color: white;
 }
 </style>
@@ -47,31 +47,29 @@
 <body>
 <!-- navbar -->
 <nav>
-<div class="logo">Web-COMS</div>
-      <input type="checkbox" id="click">
-            <label for="click" class="menu-btn">
-              <i class="fas fa-bars"></i>
-            </label>
+  <div class="logo">Web-COMS</div>
+        <input type="checkbox" id="click">
+              <label for="click" class="menu-btn">
+                <i class="fas fa-bars"></i>
+              </label>
     <ul>
     <li><a href="publicationchairhomepage.php">Back</a></li>
     <li><a class="active" href="viewcamerareadycopies.php">View Camera-Ready Copies</a></li>
     <li><a href="../../About.php">About</a></li>
     <li><a href="../../help.php">Help</a></li>
 
-
     </ul>
-    <br /><br />
   </nav>
 <h2></h2><br>
-<h2 style="color:#111 ;text-align:center;">Uploaded camera ready copies</h2><br>
-<h3 style="color:dodgerblue ;text-align:center;">As publication-chair you can download final camera-ready copies and generate proceeding preparation</h3>
+<h2 style="color:#283747 ;margin-left:20px;">Uploaded camera ready copies</h2><br>
+<!-- <p style="color:#283747 ;margin-left:20px;">As publownload final camera-ready copies and generate proceeding preparation</p> -->
 
 <table id="papersDownloads">
 <thead>
     <!-- file id -->
     <th>ID </th> 
     <!-- <th>Author's name</th> -->
-    <th>File name</th>
+    <th>Research paper</th>
     <th>Conference name</th>
     <th>University(Author)</th>
     <th>File size (in KB)</th>
@@ -96,11 +94,9 @@
 
       <td><?php echo floor($file['size'] / 1000) . ' KB'; ?></td>
       <td><?php echo $file['downloads']; ?></td>
-      <td><a style="text-decoration:none" href="viewcamerareadycopies.php?file_id=<?php echo $file['crc_id'] ?>"> Download </a></td>
       
+      <td><i class="fas fa-file-download" style="color:#1A5276;"></i><a style="text-decoration:none;color:dodgerblue;" href="viewcamerareadycopies.php?file_id=<?php echo $file['crc_id'] ?>"> Download </a></td>
 
-    
-    
     </tr>
   <?php endforeach;?>
 
