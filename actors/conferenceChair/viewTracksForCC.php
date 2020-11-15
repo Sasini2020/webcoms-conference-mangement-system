68e2490cd3d360f99ca004715f172fe23a856854
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-15 23:39:17 +0530

    Update viewTracksForCC.php

--- actors/conferenceChair/viewTracksForCC.php
+++ actors/conferenceChair/viewTracksForCC.php
@@ -8,17 +8,86 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Track List</title>
 
-  <link rel="stylesheet" href="../../css/main_style.css">
-  <link rel="stylesheet" href="../../css/table_style.css">
-  
+   <link rel="stylesheet" href="../../css/table_style.css">
+	<link rel="stylesheet" href="../../css/about_help_styles.css">
+	    <link rel="stylesheet" href="../../css/nav_footer_styles.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   <style>
+
+* {
+  font-family: sans-serif; /* Change your font family */
+}
+
+.content-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  border-radius: 5px 5px 0 0;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.content-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+  font-weight: bold;
+}
+
+.content-table th,
+.content-table td {
+  padding: 12px 15px;
+}
+
+.content-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.content-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.content-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+
+.content-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+
+.dot {
+  height: 8px;
+  width: 8px;
+  background-color: #86B0DD;
+  border-radius: 50%;
+  margin-bottom:2px;
+  margin-left:28px;
+  margin-right:5px;
+  display: inline-block;
+}
+
+</style>
 </head>
 <body>
-
-  <nav>
+	<nav>
+  <div class="logo">Web-COMS</div>
+      <input type="checkbox" id="click">
+            <label for="click" class="menu-btn">
+              <i class="fas fa-bars"></i>
+            </label>
     <ul>
-      <li><a href="viewConferencesForCC.php">Back to Conference List</a></li>
+	  <li><a class="active" href="conferencechairhomepage.php">Home</a></li>
+      <li><a href="create_conference.php">Create a Conf</a></li>
+      <li><a href="viewConferencesForCC.php">View Conf</a></li>
+      <li><a href="addnotemplates.php">Add notification templates</a></li>
+      <li><a href="upudetauls.php">Upload User Details</a></li>
+      <li style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
+
     </ul>
   </nav>
 
@@ -27,8 +96,8 @@
 <br><br>
 
 <div>
-   
-   <table>
+   <center>
+   <table class="content-table">
      <thead>
          <tr>
          <th>Number</th>
@@ -57,6 +126,7 @@
             $counter++;}
         ?> 
      </table>	
+	 </center>
  </div>
 
 
