074d220f0a75ff98009db4bb65340f35bae2b745
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-15 23:38:19 +0530

    Update defineTracks.php

--- actors/conferenceChair/defineTracks.php
+++ actors/conferenceChair/defineTracks.php
@@ -10,18 +10,43 @@
 <head>
   <title>Define Tracks</title>
 
-  <link rel="stylesheet" href="../../css/main_style.css">
+
   <link rel="stylesheet" href="../../css/sty.css">
-  
+    <link rel="stylesheet" href="../../css/table_style.css">
+	<link rel="stylesheet" href="../../css/about_help_styles.css">
+	    <link rel="stylesheet" href="../../css/nav_footer_styles.css">
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   <style>
+.dot {
+  height: 8px;
+  width: 8px;
+  background-color: #86B0DD;
+  border-radius: 50%;
+  margin-bottom:2px;
+  margin-left:28px;
+  margin-right:5px;
+  display: inline-block;
+}
+</style>
 </head>
 <body>
 
-  <nav>
+	<nav>
+  <div class="logo">Web-COMS</div>
+      <input type="checkbox" id="click">
+            <label for="click" class="menu-btn">
+              <i class="fas fa-bars"></i>
+            </label>
     <ul>
-      <li><a href="viewConferencesForCC.php">Back to Conference List</a></li>
+	  <li><a class="active" href="conferencechairhomepage.php">Home</a></li>
+      <li><a href="create_conference.php">Create a Conf</a></li>
+      <li><a href="viewConferencesForCC.php">View Conf</a></li>
+      <li><a href="addnotemplates.php">Add notification templates</a></li>
+      <li><a href="upudetauls.php">Upload User Details</a></li>
+      <li style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
+
     </ul>
   </nav>
-
 <br><br>
 
 <div id="main-wrapper">
