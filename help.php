d63152b2920cdd4bf5295d544a3cca646b9acd37
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-15 11:58:27 +0530

    Update help.php

--- help.php
+++ help.php
@@ -10,18 +10,21 @@
   </head>
   <body>
 
-    <nav>
-      <div class="logo">Web-COMS</div>
+<nav>
+	<div class="logo">Web-COMS</div>
       <input type="checkbox" id="click">
             <label for="click" class="menu-btn">
               <i class="fas fa-bars"></i>
             </label>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="About.php">About</a></li>
-                <li><a class="active" href="#">Help</a></li>
-            </ul>
-    </nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="login.php">Login</a></li>
+			<li><a href="register.php">Register</a></li>
+			<li><a class="active"  href="help.php">Help</a></li>
+			<li><a href="About.php">About</a></li>
+		</ul>
+		<br /><br />
+	</nav>
     <br><br><br><br><br><br><br><br><br> 
 
 <!-- Start of the Top content -->
