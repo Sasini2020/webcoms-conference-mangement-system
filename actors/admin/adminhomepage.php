f975f3b7dc519ef2ec97ac41a09fc2995bb42ef5
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-15 14:33:29 +0530

    Update adminhomepage.php

--- actors/admin/adminhomepage.php
+++ actors/admin/adminhomepage.php
@@ -7,53 +7,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin Home</title>
 
-	<link rel="stylesheet" href="../../css/main_style.css">
-	<link rel="stylesheet" href="../../css/style_footer.css">
-	<link rel="stylesheet" href="../../css/table_style.css">
-	
-<style>
-.p {
-	color:white;
-}
-body, html {
-  height: 100%;
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-  
-}
-
-.hero-image {
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("../../imgs/try.jpg");
-  height: 30%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-}
-.hero-text {
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: white;
-}
-
-</style>
-
-
+    <title>Conference Chair Registration</title>
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="../../css/nav_footer_styles.css">
+	<link rel="stylesheet" href="../../css/reg_form_style.css">
 </head>
 	
 
 <body>
-<div class="hero-image"> 
+
 
 	<nav>
+	
+	  <div class="logo">Web-COMS</div>
+      <input type="checkbox" id="click">
+            <label for="click" class="menu-btn">
+              <i class="fas fa-bars"></i>
+            </label>
+	
 		<ul>
 			<!--<li><a class="active" href="index.php">WebCOMS</a></li>-->
-			<li><a href="adminhomepage.php">Home</a></li>
+			<li><a class="active" href="adminhomepage.php">Home</a></li>
 			<li><a href="requested_conferences.php">Requested conferences</a></li>
 			<li><a href="conference_list.php">Conference List</a></li>
 			<li><a href="authordetails.php">Author details</a></li>
@@ -63,23 +38,8 @@ body, html {
 
 	</nav>
 	
-<div class="hero-text">
-</br>
-    <h5 style="font-size:30px">Admin</h5>
-</div>
-
-	
-	
-
-
-	
-	</br></br></br></br></br></br></br></br></br></br></br></br>
-
 <center>
-	<?php
-        include "forhomepage-reqC.php";
-	?>
-	</br></br></br></br>
+
 	<?php
         include "forhomepage-conferenceL.php";
 	?>
@@ -89,6 +49,11 @@ body, html {
 	?>
 	</br></br></br></br></br></br></br></br>
 </center>
-<h5 style="color:white; padding:20px; margin:0; text-align:center; background-color:#063247">WebCOMS @2020</h5>
+
+
+     <div class="footer">
+            <p>&copy;2020, All rights reserved by www.WebComs.lk</p>
+     </div>
+		
 </body>
 </html>
