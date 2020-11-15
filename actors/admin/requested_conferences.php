66828a460307d1dcd61ad1208d0974b060b424a6
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-11-15 14:49:56 +0530

    Update requested_conferences.php

--- actors/admin/requested_conferences.php
+++ actors/admin/requested_conferences.php
@@ -9,6 +9,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title>Requested Conferences List</title>
 
@@ -17,10 +18,8 @@
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
  
+<!-- Added css to style tag to style table -->
 <style>
-
-/* Added css to style tag to style table*/
-
 #papersDownloads {
   font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
   border-collapse: collapse;
@@ -43,6 +42,65 @@
   background-color: #5DADE2;
   color: white;
 }
+#acptBtn{
+  background-color:#4dff4d; 
+  padding:4px; 
+  radius:20px;
+  border: 2px solid #009966;
+  border-radius: 20px;
+}
+#dltBtn{
+  background-color:#ff0000; 
+  padding:5px; 
+  radius:20px;
+  border: 2px solid #009966;
+  border-radius: 20px;
+}
+
+* {
+  font-family: sans-serif; /* Change your font family */
+}
+
+.content-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  border-radius: 5px 5px 0 0;
+  overflow: hidden;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+.content-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+  font-weight: bold;
+}
+
+.content-table th,
+.content-table td {
+  padding: 12px 15px;
+}
+
+.content-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.content-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.content-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+
+.content-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+
+
 </style>
 
 </head>
@@ -51,30 +109,34 @@
   <div class="filter">
   </div>	 -->
 
-  <nav>
-  <div class="logo">Web-COMS</div>
+<nav>
+<div class="logo">Web-COMS</div>
       <input type="checkbox" id="click">
             <label for="click" class="menu-btn">
               <i class="fas fa-bars"></i>
             </label>
-    <ul>
-      <li><a class="active" href="requested_conferences.php">Requested Conferences</a></li>
-      <li><a href="../../help.php">Help</a></li>
-      <li><a href="../../About.php">About</a></li>
-      <li><a href="adminhomepage.php">Back to Home</a></li>
+		<ul>
+			<!--<li><a class="active" href="index.php">WebCOMS</a></li>-->
+			<li><a href="adminhomepage.php">Home</a></li>
+			<li><a class="active" href="requested_conferences.php">Requested conferences</a></li>
+			<li><a href="conference_list.php">Conference List</a></li>
+			<li><a href="authordetails.php">Author details</a></li>
+			<li><a href="conferenceChairRegistration.php">Conf Chair Registration</a></li>
+			<li style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
+		
 
-    </ul>    
-  </nav>
-  <br>
+	</nav>
 
-  <h2 style="margin-left:20px;"> Requested Conferences</h2>
+  <br><br>
+
+  <h2 style="text-align: center;"> Requested Conferences</h2>
 
   <br><br>
 
   <div>
     
-
-    <table id="papersDownloads">
+<center>
+    <table class="content-table"  >
       <thead>
 	      <tr>
           <th>ID</th>
@@ -88,9 +150,9 @@
           <th>Manage</th>
       	</tr>
       </thead>
-
+</center>
       <tbody>                                     
-        <?php
+                <?php
           $sql = mysqli_query($con, "select conferences.id,
             conferences.name,
             conferences.venue,
@@ -119,8 +181,8 @@
               <td style="padding-left: 20px;">
                 <form action="requested_conferences.php" method="post">
 									
-                  <input style="background-color:#196F3D;border: none;border-radius:10px;color: white;padding: 9px 20px;text-decoration: none;margin: 4px 2px;cursor: pointer;" type="submit" name="action" Value="Accept" />
-							    <input style="background-color:#E74C3C;border: none;border-radius:10px;color: white;padding: 9px 20px;text-decoration: none;margin: 4px 2px;cursor: pointer;" type="submit" name="action" value="Reject" />
+							    <input type="submit" name="action" value="Delete" />
+                  <input type="submit" name="action" Value="Accept" />
                   <input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
                 </form>
               </td>
@@ -139,7 +201,7 @@
         //  header('location:requested_conferences.php');
         //}
         if(isset($_POST['action']) && isset($_POST['id'])){
-          if($_POST['action'] == 'Reject'){
+          if($_POST['action'] == 'Delete'){
             $r_id = $_POST['id'];
             $qur = mysqli_query($con,"delete from conferences where id='$r_id'");
             header('location:requested_conferences.php');
