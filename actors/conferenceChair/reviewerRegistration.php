ee9c994e0e412b897bf596c3f4830907a421cff0
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-12-11 22:10:03 +0530

    Update reviewerRegistration.php

--- actors/conferenceChair/reviewerRegistration.php
+++ actors/conferenceChair/reviewerRegistration.php
@@ -9,3 +9,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+  <title>Reviewer Registration</title>
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="../../css/nav_footer_styles.css">
+	<link rel="stylesheet" href="../../css/reg_form_style.css">
+</head>
