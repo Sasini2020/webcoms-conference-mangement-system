65ae18063e6f57eb880d315b4732aa02e3a8fe07
Sasini <63692107+Sasini2020@users.noreply.github.com>
2020-12-23 12:17:14 +0530

    route file for reviewer

---
+++ actors/reviewer/route.php
@@ -0,0 +1,16 @@
+<?php
+
+    session_start();
+    if($_SESSION['login_s'] != '2'){
+      header('location:../../login.php');
+    }
+
+    //   if(isset($_GET['f_id']) && isset($_GET['f_title']){
+          $_SESSION['f_id'] = $_GET['f_id'];
+          $_SESSION['f_title'] = $_GET['f_title'];
+
+          header('location:addreview.php');
+        
+        
+
+?>
