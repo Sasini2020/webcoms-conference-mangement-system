9a47a561c9b2b1f30f1c15b22344b3afd8459af9
malithweerasinghe <malithrashan03@gmail.com>
2020-12-27 17:34:35 +0530

    update forget password

--- reset_request_password.php
+++ reset_request_password.php
@@ -19,32 +19,44 @@ if(isset($_POST['reset_btn'])){
     require 'dbconfig/config.php';
     
 
-  $userEmail=$_POST['email'];
-  $usertype=$_POST['user_type'];
+    $userEmail=$_POST['email'];
+    $usertype=$_POST['user_type'];
     
     $sql="DELETE FROM pwdreset WHERE resetEmail=? AND user_type=?; ";
     $stmt=mysqli_stmt_init($con);
-              if(!mysqli_stmt_prepare($stmt,$sql)){
+
+
+              if(!mysqli_stmt_prepare($stmt,$sql))
+              {
         
                      echo "There was an error";
-                    exit();
-              }else{
+                      exit();
+              }else
+                  {
 
-                       mysqli_stmt_bind_param($stmt,"ss",$userEmail,$usertype);
+                        mysqli_stmt_bind_param($stmt,"ss",$userEmail,$usertype);
                         mysqli_stmt_execute($stmt);
 
                      }
+
+
     $sql="INSERT INTO pwdreset(resetEmail,user_type,resetSelector,resetToken,resetExpires) VALUES(?,?,?,?,?);";
     $stmt=mysqli_stmt_init($con);
+
+
            if(!mysqli_stmt_prepare($stmt,$sql)){
+
                                    echo "There was an error!";
                                    exit();
               }else{
+
                             $hashedToken=password_hash($token,PASSWORD_DEFAULT);
                              mysqli_stmt_bind_param($stmt,"sssss",$userEmail,$usertype,$selector,$hashedToken,$expires);
                              mysqli_stmt_execute($stmt);
 
                     }
+
+
 
     mysqli_stmt_close($stmt);
     mysqli_close();
@@ -58,15 +70,10 @@ if(isset($_POST['reset_btn'])){
    $message .='<p>'.$usertype.'   Here is your password reset link:</br>';
    $message .='<a href="'.$url.'">'.$url.'</a></p>';
 
-
-  
-
- 
-
    $headers="From: webcoms <webcoms2020@gmail.com>\r\n";
    
- $headers .="Reply-To: webcoms2020@gmail.com \r\n";
- $headers .="Content-type:text/html \r\n";
+   $headers .="Reply-To: webcoms2020@gmail.com \r\n";
+   $headers .="Content-type:text/html \r\n";
  
    mail($to,$subject,$message,$headers);
 
