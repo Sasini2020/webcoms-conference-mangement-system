74aa5930ff7854b4bb854b50e8c40fe4fbea31ee
Sasini <63692107+Sasini2020@users.noreply.github.com>
2020-12-31 19:52:23 +0530

    Finalized conf guidelines view part for author

--- actors/author/author_home.php
+++ actors/author/author_home.php
@@ -5,7 +5,7 @@
     }
     require '../../dbconfig/config.php';
 ?>
-<?php include 'fileLogicForViewConfGuidelines.php';?>
+<?php //include 'fileLogicForViewConfGuidelines.php';?>
 
 <!DOCTYPE html>
 <html>
@@ -110,22 +110,18 @@ $(document).ready(function(){
 
 
 
-  <nav>
+<nav>
 <div class="logo">Web-COMS</div>
       <input type="checkbox" id="click">
             <label for="click" class="menu-btn">
               <i class="fas fa-bars"></i>
             </label>
-	<ul>
-    
-	  <li><a class="active" href="author_home.php">home</a></li>
-<!--           <li><a href="../../About.php">About</a></li> -->
-<!--           <li><a href="../../help.php">Help</a></li> -->
-    <!-- <li><a href="author_change_password.php">Change Password</a></li>
-	 <li><a href="updateprofile.php">Update Profie</a></li>-->
-         <li ><a style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
+	<ul>  
+	  <li><a class="active" href="author_home.php">Home</a></li> 
+	  <!-- <li><a href="updateprofile.php">Update Profie</a></li> -->
+    <li ><a style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
 	</ul>
-  </nav>
+</nav>
 
   <br>
   <h2 style="margin-left:20px;">All Conferences </h2>
@@ -174,9 +170,9 @@ $(document).ready(function(){
 		while ($row = $result-> fetch_assoc()){
 
 	    echo "</tr><td>". $row["name"] ."</td><td>". $row["venue"] ."</td><td>". $row["start_date"] ."</td><td>". $row["end_date"];
-      echo "<td><a href='route.php?viewConfGuidelines=". $row['id'] ."' class='conListLink'>View</a></td>";
+      echo "<td><a href='routeforConfGuidelines.php?Conf_Id=". $row['id'] . "&Conf_Name=". $row['name'] . " '    class='conListLink' > View </a></td>";
 
-      echo "<td><a href='papersubmission.php?c_id=". $row['id'] ."' title='submit paper' class='linkDec'><span style='margin-right:5px;'><i class='fas fa-file-upload'></i></span>Submit</a></td>";
+      echo "<td><a href='papersubmission.php?c_id=". $row['id'] ." ' title='submit paper' class='linkDec'><span style='margin-right:5px;'><i class='fas fa-file-upload'></i></span>Submit</a></td>";
 
       echo "<td><a href='route.php?viewRPaper_cid=". $row['id'] ."' class='conListLink'>View</a></td>";
 		
@@ -202,6 +198,105 @@ $(document).ready(function(){
 	<div class="footer">
             <p>&copy;2020, All rights reserved by www.WebComs.lk</p>
 	</div>
+
+
+<!-- FilesLogic is here -->
+
+  <?php
+// connect to the database
+$conn = $con;
+
+$c_id = $_SESSION['c_id'];
+
+$sql = "SELECT * FROM conferenceguidelinedetails where conf_id='$c_id'";
+
+
+$result = mysqli_query($conn, $sql);
+
+$files = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// Uploads files
+if (isset($_POST['save'])) { // if save button on the form is clicked
+    // name of the uploaded file
+    $filename = $_FILES['myfile']['name'];
+
+    // destination of the file on the server
+    $destination = '../../uploads/conferenceGuidelines/' . $filename;
+
+    // get the file extension
+    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+    // the physical file on a temporary uploads directory on the server
+    $file = $_FILES['myfile']['tmp_name'];
+    $size = $_FILES['myfile']['size'];
+
+
+    if (!in_array($extension, ['pdf'])) {
+        echo '<script type="text/javascript"> alert("Your file extension must be .pdf") </script>';
+    } elseif ($_FILES['myfile']['size'] > 1000000) { // file shouldn't be larger than 1MB
+        // echo "File is large than 1MB !";
+        echo '<script type="text/javascript"> alert("file size larger than 1 MB.. Try another file") </script>';
+
+    } else {
+        // move the uploaded (temporary) file to the specified destination
+        if(move_uploaded_file($file, $destination)) {
+
+        $c_id = $_SESSION['c_id'];
+
+
+            $query1="select * from conferenceguidelinedetails where conf_id='$c_id' ";
+            $query1_run=mysqli_query($con,$query1);
+            if(mysqli_num_rows($query1_run)>0){
+                echo '<script type="text/javascript">alert("This conference has been already uploaded conference guidelines..!!")</script>';
+            }
+            else{
+
+                $sql = "INSERT INTO conferenceguidelinedetails(name,size,conf_id) VALUES 
+                ('$filename',$size,$c_id)";
+               
+                if (mysqli_query($conn, $sql)) {
+                    // echo "File uploaded successfully";
+                    echo '<script type="text/javascript"> alert("Conference Guidelines was uploaded successfully!!") </script>';
+    
+                }
+            }
+
+
+        }
+         else {
+            echo '<script type="text/javascript"> alert("Failed to submit your file !!") </script>';
+        }
+    }
+}
+// Downloads files
+if (isset($_GET['file_id'])) {
+    $id = $_GET['file_id'];
+
+    // fetch file to download from database
+    $sql = "SELECT id,name,size FROM conferenceguidelinedetails WHERE id=$id  ";
+    $result = mysqli_query($conn, $sql);
+
+    $file = mysqli_fetch_assoc($result);
+    $filepath = '../../uploads/conferenceGuidelines/' . $file['name'];
+
+    if (file_exists($filepath)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($filepath));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize('../../uploads/conferenceGuidelines/' . $file['name']));
+        readfile('../../uploads/conferenceGuidelines/' . $file['name']);
+
+        exit;
+    }
+
+}
+?>
+
+
+
+
 
 </body>
 </html>
