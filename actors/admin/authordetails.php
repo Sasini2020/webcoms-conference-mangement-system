e90fc68edfb8e63dd2892f9067e0610e0cace88a
malithweerasinghe <malithrashan03@gmail.com>
2021-01-02 11:37:11 +0530

    author details pdf download

--- actors/admin/authordetails.php
+++ actors/admin/authordetails.php
@@ -99,7 +99,6 @@
 <br><br><br><br>
 <center>
   
-<!--<input  class="btn" type="button" id="btnExport" value="Report Download" name="rest" /><br><br>-->
 <button class="btn" id="btnex" >  Report Download    <i class="fa fa-download"></i></button>
 	<table id="dpdf" class="content-table">
 	
@@ -140,6 +139,32 @@
 	?>
 	
 	</table>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
+  
+  
+
+ 
+  <script type="text/javascript">
+        $("body").on("click", "#btnex", function () {
+            html2canvas($('#dpdf')[0], {
+                onrendered: function (canvas) {
+                    var data = canvas.toDataURL();
+                    var docDefinition = {
+                        content: [{
+                            image: data,
+                            width: 500
+                        }]
+                    };
+                    pdfMake.createPdf(docDefinition).download("author-details.pdf");
+                }
+            });
+        });
+    </script>
+ 
+
+
 	
 
 	<br/>	
