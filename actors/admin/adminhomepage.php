1e12e4c74e41a98612f0fada6a83ca52f74ae586
dhanushkagimhan <dhanushkagimhan@gmail.com>
2021-01-10 13:25:09 +0530

    starting to add update and chenge pass section

--- actors/admin/adminhomepage.php
+++ actors/admin/adminhomepage.php
@@ -12,6 +12,7 @@
 	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="../../css/nav_footer_styles.css">
 	<link rel="stylesheet" href="../../css/reg_form_style.css">
+	<link rel="stylesheet" href="../../css/DropDownListToNav.css">
 </head>
 	
 
@@ -35,8 +36,16 @@
 			<li><a href="conferenceChairRegistration.php">Conf Chair Registration</a></li>
 			<li><a href="conferenceTrackDefine.php">Conf Track Defination</a></li>
 		        <!--<li><a href="updateprofile.php">Update Profie</a></li>
-			<li><a href="admin_change_password.php">Change Password</a></li>-->
-			<li style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
+			<li><a href="admin_change_password.php">Change Password</a></li>   -->
+			<li class="dropdown">				
+					<a href="#" class="dropdown">Profile <i class="fa fa-caret-down"></i></a>
+					
+					<div class="dropdown-content">
+						<a href="updateprofile.php">Update profile</a>
+						<a href="#">Link 2</a>
+						<a href="../logout.php">Log Out</a>
+					</div>
+			</li>
 		</ul>
 
 	</nav>
--- actors/admin/updateprofile.php
+++ actors/admin/updateprofile.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  
+  require '../../dbconfig/config.php';
   if($_SESSION['login_s'] != '1'){
     header('location:../../login.php');
   }
@@ -71,14 +71,14 @@
 	
 		<ul>
 			<!--<li><a class="active" href="index.php">WebCOMS</a></li>-->
-			<li><a class="active" href="adminhomepage.php">Home</a></li>
-			<li><a href="requested_conferences.php">Requested conferences</a></li>
+			<li><a class="active" href="adminhomepage.php">Back to Home</a></li>
+			<!--<li><a href="requested_conferences.php">Requested conferences</a></li>
 			<li><a href="conference_list.php">Conference List</a></li>
 			<li><a href="authordetails.php">Author details</a></li>
 			<li><a href="conferenceChairRegistration.php">Conference Chair Registration</a></li>
 			<li><a href="conferenceTrackDefine.php">Conference Track Defination</a></li>
 		        <li><a href="updateprofile.php">Update Profie</a></li>
-			<!--<li><a href="admin_change_password.php">Change Password</a></li>-->
+			<li><a href="admin_change_password.php">Change Password</a></li>-->
 			<li style="float:right; margin-right:40px"><a href="../logout.php">Log Out</a></li>
 		</ul>
 
@@ -92,19 +92,52 @@
 			<br><h1>Update Profile</h1>
 			<fieldset>
       		<legend><span class="number"></span>Your Basic Information</legend><br>
+
+      <?php
+        $email=$_SESSION['ad_email'];
+        $user_type='Admin';
+
+        $query_result = mysqli_query($con,"select * from userinfotable where (email='$email') and (user_type='$user_type')");
+        $row = mysqli_fetch_assoc($query_result);
+      ?>
 			
       <label for="aTitle">Title:</label><br>
+      <?php
+          switch($row['title']){
+            case "Mr":
+              $op0 = "";$op1 = "selected";$op2 = "";$op3 = "";$op4 = "";$op5 = "";$op6 = "";
+              break;
+            case "Ms":
+              $op0 = "";$op1 = "";$op2 = "selected";$op3 = "";$op4 = "";$op5 = "";$op6 = "";
+              break;
+            case "Mrs":
+              $op0 = "";$op1 = "";$op2 = "";$op3 = "selected";$op4 = "";$op5 = "";$op6 = "";
+              break;
+            case "Miss":
+              $op0 = "";$op1 = "";$op2 = "";$op3 = "";$op4 = "selected";$op5 = "";$op6 = "";
+              break;
+            case "Prof":
+              $op0 = "";$op1 = "";$op2 = "";$op3 = "";$op4 = "";$op5 = "selected";$op6 = "";
+              break;
+            case "Dr":
+              $op0 = "";$op1 = "";$op2 = "";$op3 = "";$op4 = "";$op5 = "";$op6 = "selected";
+              break;
+            default:
+              $op0 = "selected";$op1 = "";$op2 = "";$op3 = "";$op4 = "";$op5 = "";$op6 = "";
+          }
+        ?>
       <select name="acTitle" id="aTitle">
-        <option value="Mr">Mr.</option>
-        <option value="Ms">Ms.</option>
-        <option value="Mrs">Mrs.</option>
-        <option value="Miss">Miss.</option>
-        <option value="Prof">Prof.</option>
-        <option value="Dr">Dr.</option>
+        <option value="" <?= $op0 ?>>No selected</option>
+        <option value="Mr" <?= $op1 ?>>Mr.</option>
+        <option value="Ms" <?= $op2 ?>>Ms.</option>
+        <option value="Mrs" <?= $op3 ?>>Mrs.</option>
+        <option value="Miss" <?= $op4 ?>>Miss.</option>
+        <option value="Prof" <?= $op5 ?>>Prof.</option>
+        <option value="Dr" <?= $op6 ?>>Dr.</option>
       </select>
       
       <label for="fname">Full Name:</label><br>
-			<input id="fname" name="fullname" type="text" class="inputvalues" placeholder="Type your Full Name" required/><br>
+			<input id="fname" name="fullname" type="text" class="inputvalues" placeholder="<?= $row['full_name'] ?>"/><br>
 			
 	 <!--<label for="email">Email:</label><br>
 			<input id="email" name="email" type="text" class="inputvalues" placeholder="Type your current Email" required/><br>-->
@@ -112,70 +145,47 @@
 
 
       <label for="aOrganization">Organization:</label><br>
-			<input id="aOrganization" name="Organization" type="text" class="inputvalues" placeholder="Type your Organization" /><br>
+			<input id="aOrganization" name="Organization" type="text" class="inputvalues" placeholder="<?= $row['organization'] ?>" /><br>
 
       <br>
 			
 
 			<label for="ContactDetails">Contact No:</label><br>
-			<input id="ContactDetails"  name="ContactDetails" type="tel" class="inputvalues" pattern="[0-9]{1}[0-9]{9}" placeholder="Type your Contact Number" title="Phone number with 0-9 and remaing 9 digit with 0-9"required/>
+			<input id="ContactDetails"  name="ContactDetails" type="tel" class="inputvalues" pattern="[0-9]{1}[0-9]{9}" placeholder="<?= $row['contactdetails'] ?>" title="Phone number with 0-9 and remaing 9 digit with 0-9"/>
       
     
       <br>
 		</fieldset>
 
 	
-			<button name="submit_btn" type="submit" id='btnValidate' value="Sign Up" >UPDATE</button><br>
+			<button name="update_btn" type="submit" id='btnValidate' value="Sign Up" >UPDATE</button><br>
 
 		</form>
 
 
 
 		<?php
-			if(isset($_POST['submit_btn']))
+			if(isset($_POST['update_btn']))
 			{
-        require '../../dbconfig/config.php';
-          $email=$_SESSION['ad_email'];
+        
+          
           $aTitle = $_POST['acTitle'];
           $fullname =$_POST['fullname'];
 				  $Organization = $_POST['Organization'];
 				  $ContactDetails = $_POST['ContactDetails'];
-          
-          $query="SELECT * from userinfotable WHERE email=? AND user_type=?;";
-              
-          $user_type='Admin';
-         $stmt=mysqli_stmt_init($con);
 
-         if(!mysqli_stmt_prepare($stmt,$query)){
-                                   
-          echo "There was an error2";
-          exit();
-        }else{
-          mysqli_stmt_bind_param($stmt,"ss",$email,$user_type);
-          mysqli_stmt_execute($stmt);
-          $result=mysqli_stmt_get_result($stmt);
-
-          if(!$row=mysqli_fetch_assoc($result) ){
-                         
-              echo "There was a error3!";
-              exit();
-          }else{
-
-                $query = "UPDATE userinfotable SET full_name = '$fullname',
-                organization = '$Organization', contactdetails = '$ContactDetails'
-                WHERE email = '$email' AND user_type='$user_type'";
-                $query_run = mysqli_query($con,$query);
+          $query = "UPDATE userinfotable SET full_name = '$fullname',
+          organization = '$Organization', contactdetails = '$ContactDetails'
+          WHERE email = '$email' AND user_type='$user_type'";
+          $query_run = mysqli_query($con,$query);
 				
-				if($query_run)
-                {
+				if($query_run){
                   echo '<script type="text/javascript"> 
                      alert("Update Successfully.");
-                  </script>';
-                }
-			
-          }
-        } 
+                  </script>';		
+        }
       }
+      
         ?>
 	</div>
 
